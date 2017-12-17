92a1a7e5d4177153c80cc8ca7a77211122300ca7
Adrien Mousty <moustyadrien@hotmail.com>
2017-12-17 13:23:10 +0100

    Generation data chart from JSON effective

--- js/reporting-graph.js
+++ js/reporting-graph.js
@@ -1,6 +1,6 @@
 var TABCOMPARATEDVALUE = ["Age", "BMI", "HbA1c (ratio)", "Total cholesterol (mg/dl)", "HDL cholesterol (mg/dl)", "Systolic blood pressure (mmHg)", "Smoking consumption (packs/year)"];
 // #TODO: DELETE THIS SHIT AFTER TEST
-var TABUSRID = [1,2];
+var TABUSRID = [1,53];
 var OBJ_USERS = [];
 
 var color = Chart.helpers.color;
@@ -8,80 +8,11 @@ var barChartData = "";
 
 /*  DCUMENT READY */
 $( document ).ready( function() {
-    GetJsonInObject(TABUSRID);
-    generateChartData();
-    
-    var ctx = $("#canvas")[0].getContext("2d");
-    window.myBar = new Chart(ctx, {
-        type: 'bar',
-        data: barChartData,
-        options: {
-            responsive: true,
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Risk viewer comparison'
-            }
-        }
-    });
-
-
-    /* ONCLICK */
-    $('#randomizeData').click(function() {
-        var zero = Math.random() < 0.2 ? true : false;
-        barChartData.datasets.forEach(function(dataset) {
-            dataset.data = dataset.data.map(function() {
-                return zero ? 0.0 : randomScalingFactor();
-            });
-
-        });
-        window.myBar.update();
-    });
-
-    var colorNames = Object.keys(window.chartColors);
-    $('#addDataset').click(function() {
-        var colorName = colorNames[barChartData.datasets.length % colorNames.length];;
-        var dsColor = window.chartColors[colorName];
-        var newDataset = {
-            label: 'Dataset ' + barChartData.datasets.length,
-            backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-            borderColor: dsColor,
-            borderWidth: 1,
-            data: []
-        };
-
-        for (var index = 0; index < barChartData.labels.length; ++index) {
-            newDataset.data.push(randomScalingFactor());
-        }
-
-        barChartData.datasets.push(newDataset);
-        window.myBar.update();
-    });
-
-    $('#addData').click(function() {
-        if (barChartData.datasets.length > 0) {
-            var ComparatedValues = TABCOMPARATEDVALUE[barChartData.labels.length % TABCOMPARATEDVALUE.length];
-            barChartData.labels.push(ComparatedValues);
-
-            for (var index = 0; index < barChartData.datasets.length; ++index) {
-                //window.myBar.addData(randomScalingFactor(), index);
-                barChartData.datasets[index].data.push(randomScalingFactor());
-            }
-
-            window.myBar.update();
-        }
-    });
-
-    $('#removeDataset').click(function() {
-        barChartData.datasets.splice(0, 1);
-        window.myBar.update();
-    });
+    generateFullChartFromJSON(TABUSRID);
 });
 
 /* JSON RELATED FUNCTIONS */
-function GetJsonInObject(tabIdUSer){
+function generateFullChartFromJSON(tabIdUSer){
   // 1. Variables
   var indexUsr = 0;
   // 2. Search if given user exist in json file
@@ -89,10 +20,12 @@ function GetJsonInObject(tabIdUSer){
     for(indexUsr = 0; indexUsr < tabIdUSer.length; indexUsr++){
       $.each( data, function( index, value ) {
         if(tabIdUSer[indexUsr] == value.id){
-          // Return found user as object
+          // Return found user as object, increment array of users
           OBJ_USERS[indexUsr] = generateSingleUserData(value);
         }
       });
+      // Fill data to the chart
+      generateChartData();
     }
   });
 }
@@ -101,6 +34,7 @@ function generateSingleUserData(singleUserData){
   /*"Age", "BMI", "HbA1c (ratio)", "Total cholesterol (mg/dl)", "HDL cholesterol (mg/dl)", "Systolic blood pressure (mmHg)", "Smoking consumption (packs/year)"*/
   var user = [];
   user.id = singleUserData.id;
+  user.fullname  = singleUserData.admin.nom.toUpperCase() + " " + singleUserData.admin.prenom;
   user.age = getAge(singleUserData.admin.date_de_naissance);
   user.bmi = getBmi(singleUserData.biometrie.poids, singleUserData.biometrie.taille);
   user.HbA1c = singleUserData.const_biologique.HbA1c;
@@ -114,51 +48,50 @@ function generateSingleUserData(singleUserData){
 /* CHART GENERATION DATA RELATED FUNCTION */
 function generateChartData(){
   barChartData = {
-      labels: ["Age", "BMI", "HbA1c (ratio)", "Total cholesterol (mg/dl)", "HDL cholesterol (mg/dl)", "Systolic blood pressure (mmHg)", "Smoking consumption (packs/year)"],
-      /*datasets: [{
-          label: 'Username 1',
-          backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-          borderColor: window.chartColors.red,
-          borderWidth: 1,
-          data: [
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor()
-          ]
-      }, {
-          label: 'Username 2',
-          backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
-          borderColor: window.chartColors.blue,
-          borderWidth: 1,
-          data: [
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor(),
-              randomScalingFactor()
-          ]
-      }]*/
-
+      labels: TABCOMPARATEDVALUE,
+      datasets : []
   }; // End barchardata
 
   // DATA GENERATION
   for(var i = 0; i < OBJ_USERS.length; i++){
-      var ComparatedValues = TABCOMPARATEDVALUE[barChartData.labels.length % TABCOMPARATEDVALUE.length];
-      barChartData.labels.push(ComparatedValues);
-
-      for (var index = 0; index < barChartData.datasets.length; ++index) {
-          //window.myBar.addData(randomScalingFactor(), index);
-          barChartData.datasets[index].data.push(OBJ_USERS[index]);
+      var myNewDataset = {
+        label : OBJ_USERS[i].fullname,
+        backgroundColor : color(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor : window.chartColors.blue,
+        borderWidth : 1 ,
+        data : [
+          OBJ_USERS[i].age ,
+          OBJ_USERS[i].bmi ,
+          OBJ_USERS[i].HbA1c ,
+          OBJ_USERS[i].Cholesterol_total ,
+          OBJ_USERS[i].Cholesterol_HDL ,
+          OBJ_USERS[i].PSS ,
+          OBJ_USERS[i].Consommation_tabagique
+        ]
       }
-
-      window.myBar.update();
+      barChartData.datasets.push(myNewDataset);
+      //myBarChart.chart.config.data.datasets.push({ myNewDataset });
   }
+  //window.myBarChart.update();
+  generateChart();
+}
+
+function generateChart(){
+  var ctx = $("#canvas")[0].getContext("2d");
+  window.myBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: barChartData,
+      options: {
+          responsive: true,
+          legend: {
+              position: 'top',
+          },
+          title: {
+              display: true,
+              text: 'Risk viewer comparison'
+          }
+      }
+  });
 }
 /* HELPERS #TODO : DEPLACE THIS TO HELPER.JS*/
 /* https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd */
