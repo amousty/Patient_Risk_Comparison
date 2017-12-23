7991b1ec49157900de908f04d8b2fce8d7feb7ae
Adrien Mousty <moustyadrien@hotmail.com>
2017-12-23 09:56:10 +0100

    Add tooltip & doc
    - Add user-manual localisation
    - Add few helpfull tooltips
    - Remove some usless code

--- js/start.js
+++ js/start.js
@@ -1,28 +1,3 @@
-var $headline = $('.headline'),
-    $inner = $('.inner'),
-    $nav = $('nav'),
-    navHeight = 75;
-
-$(window).scroll(function() {
-  if ($nav.length) {
-    var scrollTop = $(this).scrollTop(),
-        headlineHeight = $headline.outerHeight() - navHeight,
-        navOffset = $nav.offset().top;
-  }
-
-  $headline.css({
-    'opacity': (1 - scrollTop / headlineHeight)
-  });
-  $inner.children().css({
-    'transform': 'translateY('+ scrollTop * 0.4 +'px)'
-  });
-  if (navOffset > headlineHeight) {
-    $nav.addClass('scrolled');
-  } else {
-    $nav.removeClass('scrolled');
-  }
-});
-
 /*
     DOCUMENT READY & LOAD
 */
@@ -30,6 +5,12 @@ $( document ).ready( function() {
   $('#loader').show();
   $('#data-container').hide();
 
+  // Tooltip from bootstrap
+  $('[data-toggle="tooltip"]').tooltip({
+    trigger : 'hover'
+  });
+
+  // When the user click on CHART or GRAPH on header, scrolling is more user friendly
   $('.smooth-scrolling').on('click', function() {
       // Target page
 			var page = $(this).attr('href');
@@ -39,6 +20,7 @@ $( document ).ready( function() {
 			return false;
 		});
 
+    // Unslect patient, btn visible from modal
     $('#btn-rmv-patient').click(function(){
       removePatient();
     });
@@ -51,5 +33,6 @@ $( window ).on( "load", function() {
     // page is fully loaded, including all frames, objects and images
     // We pass the IDs chosen by the doctor
     $('#data-container').show('slow');
+    // Table is generated
     loadTableData();
 });
